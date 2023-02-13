6d0edfd8d5a74f4fcc383553c7269edb9f12ea01
ZhengHui <lim_z_hui@hotmail.com>
2023-02-13 10:59:46 +0800

    Simple guess game

--- src/main/java/sg/edu/nus/iss/app/App.java
+++ src/main/java/sg/edu/nus/iss/app/App.java
@@ -1,9 +1,34 @@
 package sg.edu.nus.iss.app;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class App 
 {
     public static void main( String[] args )
     {
+
+        Random random = new Random();
+        Integer randomNumber = random.nextInt(100);
+
+        Integer myGuess = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.printf("secret number: %d\n", randomNumber);
+
+        while (myGuess != randomNumber) {
+            myGuess = scanner.nextInt();
+
+            if (myGuess < randomNumber) {
+                System.out.println("Your guess is lower");
+            } else if (myGuess > randomNumber) {
+                System.out.println("Your guess is higher.");
+            } else {
+                System.out.println("You guessed it right!");
+                scanner.close();
+            }
+        }
         
         
     }
